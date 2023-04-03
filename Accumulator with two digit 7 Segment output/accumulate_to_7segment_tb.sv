6d5d0905e7a99dabd0e6193ade5d2bd288936658
Santhush K <skwin95@gmail.com>
2023-04-03 10:24:37 +0530

    Accumulator finished

--- Accumulator with two digit 7 Segment output/accumulate_to_7segment_tb.sv
+++ Accumulator with two digit 7 Segment output/accumulate_to_7segment_tb.sv
@@ -8,6 +8,7 @@ module accumulator_to_7segment;
   	logic [W-1:0] s_data = 0;
     logic [1:0][6:0] m_data;
   
+    logic [$clog2(N)-1: 0] tb_c_data = 0;
   	logic [$clog2(99)-1: 0] tb_sum_data = 0;
   	logic [1:0][6:0] tb_m_data;
   	logic [9:0][6:0] lut_decimal_to_7segment = 
@@ -20,20 +21,25 @@ module accumulator_to_7segment;
     //Driver
     initial begin
       	$dumpfile("dump.vcd"); $dumpvars(0, dut);
-        repeat (2) @(posedge clk) #1
-        rstn = 1;
+        @(posedge clk) #1 rstn = 1;
       repeat (100) @(posedge clk) begin 
-          #1 s_data <= $urandom();
-          tb_sum_data <= (tb_sum_data + s_data > 'd99) ? '0 : (tb_sum_data + s_data);
-      	end       	
-        $finish();
+        #1 s_data <= $urandom();
+        if (!rstn) begin
+          tb_c_data <= '0;
+          tb_sum_data <= '0;
+        end   
+      	else begin 
+          tb_c_data <= (tb_c_data == N-1) ? '0 : tb_c_data + 1'b1;
+          tb_sum_data <= (tb_c_data == N-1) || (tb_sum_data + s_data > 'd99) ? '0 : (tb_sum_data + s_data);
+        end
+      end       	
+      $finish();
     end
 
     //Monitor
     initial forever begin
-        @(posedge clk)
-      tb_m_data[0] = lut_decimal_to_7segment[(tb_sum_data%10)]; 
-      tb_m_data[1] = lut_decimal_to_7segment[(tb_sum_data/10)];
+      @(posedge clk)
+      tb_m_data = decimal_to_7segment(tb_sum_data);
       assert ({tb_m_data[1], tb_m_data[0]} == {m_data[1], m_data[0]}) $display("Ok, %b", {m_data[1], m_data[0]});
       else $error("TB_Sum %b, Design_sum %b", {tb_m_data[1], tb_m_data[0]}, {m_data[1], m_data[0]});
     end
